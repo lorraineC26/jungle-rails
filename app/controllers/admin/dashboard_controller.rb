be2c8a842b55b74d542a218c6dd6c085d4f8342c
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-17 00:46:56 -0500

    add Admin Dashboard Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate
+
   def show
+    @num_of_products = Product.count
+    @num_of_categories = Category.count
   end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["ADMIN_AUTH_USERNAME"] && password == ENV["ADMIN_AUTH_PASSWORD"]
+    end
+  end
+
 end

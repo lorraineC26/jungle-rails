08aaa867402ce5e9b39ce596d5c786d1f260c89a
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-19 18:37:43 -0500

    add user controller; new create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # provided by Rails and handles password encryption using bcrypt
+  has_secure_password
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
 end

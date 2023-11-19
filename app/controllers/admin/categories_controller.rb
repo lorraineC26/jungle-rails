41f2fb1fbe07e1d3120be3848e557f0d12867c28
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-19 01:40:43 -0500

    add admin new cat page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,12 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    #retrieves all records from the Category model,
+    #orders them by the name attribute in ascending order.
     @categories = Category.order(:name).all
   end
 
   def new
+    @category = Category.new
   end
 
   def create

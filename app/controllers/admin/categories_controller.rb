7888c4f71b2e96433fdc0beb064e5588ea28286c
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-19 01:47:28 -0500

    add create action

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CategoriesController < ApplicationController
+  
+  before_action :authenticate
+
   def index
     #retrieves all records from the Category model,
     #orders them by the name attribute in ascending order.
@@ -10,6 +13,25 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["ADMIN_AUTH_USERNAME"] && password == ENV["ADMIN_AUTH_PASSWORD"]
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end

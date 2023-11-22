0e6f808b8cefdd7a8150c6fc128477e3bac44ba6
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-21 21:03:19 -0500

    populate products for test db

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -43,19 +43,3 @@ private
 def open_asset(file_name)
   File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
-
-# CypressRails.hooks.before_server_start do
-#   # Called once, before either the transaction or the server is started
-# end
-
-# CypressRails.hooks.after_transaction_start do
-#   # Called after the transaction is started (at launch and after each reset)
-# end
-
-# CypressRails.hooks.after_state_reset do
-#   # Triggered after `/cypress_rails_reset_state` is called
-# end
-
-# CypressRails.hooks.before_server_stop do
-#   # Called once, at_exit
-# end
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,11 +1,20 @@
+// run `rails server -e test` in ruby test server mode
 describe('visit the homepage', () => {
 
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
 
+  it("There is nav bar on top", () => {
+    cy.get(".navbar").should("be.visible");
+  })
+
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 })

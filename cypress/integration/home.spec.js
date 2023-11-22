c89c68f7bde9febe9b01bba4cf572813fdf9946b
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-21 20:35:34 -0500

    test pdts on the page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe('visit the homepage', () => {
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+})
